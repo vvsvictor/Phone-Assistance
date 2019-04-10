24bffce53ac8bfdf17d879ddded6c992c495cd84
Daesca99 <daesca@bemen3.cat>
2019-04-10 18:48:28 +0200

    Add all js in index.php

--- frontend/index.php
+++ frontend/index.php
@@ -5,7 +5,9 @@
   <title>Phone Assistence</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/login.css">
-  <script src="index.js"></script>
+  <script src="/js/jquery-1.12.4.js"></script>
+  <script src="/js/jquery-ui.js"></script>
+  <script src="/js/index.js"></script>
 </head>
 
 <body>
