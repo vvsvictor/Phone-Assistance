4681b345dc50adbaaef9986887983cf7f401fbcb
Sengi <martinez.garcia.jordi@gmail.com>
2019-04-10 18:49:42 +0200

    Updated DB

--- DataBase/database.sql
+++ DataBase/database.sql
@@ -22,28 +22,36 @@ CREATE TABLE PERSONAL_CARD(
   gender varchar(255),
   language varchar(255),
   birthdate date,
-  dni varchar(50),
+  dninif varchar(50) unique,
   province varchar(255),
   comarca varchar(255),
   municipality varchar(255),
-  adress int not null,
+  address varchar(255),
   ownership varchar(255),
-  telephone varchar(50),
   phone varchar(50),
+  mobile_phone varchar(50),
   work_phone varchar(50),
-  id_user int not null,
-  primary key(id),
-  FOREIGN key (id_user) REFERENCES users(id)
+  primary key(id)
 );
 
 CREATE TABLE CAP(
   id int not null auto_increment,
   name varchar(255),
-  gender_doctor varchar(10),
-  doctor_name varchar(255),
-  health_insurance_id int,
+  address varchar(255),
+  phone int,
+  address varchar(255),
+  primary key(id)
+);
+
+CREATE TABLE DOCTORS(
+  id int not null auto_increment,
+  name varchar(255),
+  surname varchar(255),
+  gender varchar(10),
+  specialization varchar(255),
+  id_cap int not null,
   primary key(id),
-  FOREIGN key (health_insurance_id) REFERENCES HEALTH_INSURANCE(id)
+  FOREIGN key (id_cap) REFERENCES CAP(id)
 );
 
 CREATE TABLE HEALTH_INSURANCE(
@@ -57,19 +65,29 @@ CREATE TABLE STA(
   id int not null auto_increment,
   actual_situation varchar(50),
   hiring_date date,
-  contracted_services varchar(255),
+  tf_service bit,
+  tcr_service bit,
+  cc_service bit,
+  tm_service bit,
+  tam_service bit,
+  gps_service bit,
+  umt_service bit,
   primary key (id)
-  );
+);
 
 CREATE TABLE RESPONSIBLE(
   id int not null auto_increment,
   user_vinculation_id int not null,
-  name_surname varchar(255),
+  user_dninif not null varchar(255),
+  priority int not null,
+  name varchar(255),
+  surname varchar(255),
+  address varchar(255),
   post_code varchar(8),
   contact_phone varchar(20),
   preferable_hour varchar(255),
   date_responsible date,
-  reason varchar(255),
+  reason varchar(max),
   primary key (id),
   FOREIGN key (user_vinculation_id) REFERENCES PERSONAL_CARD(id)
 );
@@ -80,6 +98,7 @@ CREATE TABLE CALL_HISTORY(
   call_type varchar(100),
   outcall_type varchar(100),
   incall_type varchar(100),
-  teleoperator_solution varchar(255),
+  state varchar(25),
+  teleoperator_solution varchar(max),
   primary key(id)
 );
