e446635920f1dee63ec69f32898b15d22093207f
Sengi <martinez.garcia.jordi@gmail.com>
2019-04-29 19:41:31 +0200

    Style of jqgrid changed to boostrap

--- frontend/users.php
+++ frontend/users.php
@@ -7,15 +7,17 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="index.css">
-        <link rel="stylesheet" href="./js/jqGrid/css/ui.jqgrid.css">
-        <!-- Pendiente de cambiarlo con el de bootstrap -->
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" />
 
+
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="./js/jqGrid/css/ui.jqgrid.css">
+        <link rel="stylesheet" href="./js/jqGrid/css/ui.jqgrid.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="./js/index.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="./js/jqGrid/js/jquery.jqgrid.src.js"></script>
+        <script src="./js/jqGrid/js/jquery.jqgrid.min.js"></script>
 
 
         <script>
@@ -49,11 +51,14 @@
                 pager: '#pager',
                 gridview: true,
                 ignoreCase: true,
-                rownumbers: false,
+                rownumbers: true,
                 sortname: 'invdate',
                 viewrecords: true,
                 sortorder: 'desc',
-                height: '100%'
+                guiStyle: "bootstrap",
+                iconSet: "fontAwesome",
+                idPrefix: "gb1_",
+                //caption: "The grid, which uses predefined formatters and templates" <-- Minimizado de tabla completa
             });
             jQuery(myGrid).jqGrid('filterToolbar', { stringResult: true, searchOnEnter: false, defaultSearch: "cn" });
 
