d411eff184947044f75885d6a306b2f7bb5b52f9
Sengi <martinez.garcia.jordi@gmail.com>
2019-05-13 20:37:20 +0200

    add link to the image

--- frontend/users.php
+++ frontend/users.php
@@ -20,7 +20,7 @@ include '..\backend\includeAdmin.php';
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #11999E;">
-      <a class="navbar-brand" href="#"><img src="images/PA-Mini.png"/></a>
+      <a class="navbar-brand" href="index.php"><img src="images/PA-Mini.png"/></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
