c7b38a69b7b8ec23a8ae3562befb3715b440e204
Daesca99 <daesca@bemen3.cat>
2019-05-13 20:52:45 +0200

    Add BD gets

---
+++ backend/selects/getLanguages.php
@@ -0,0 +1,23 @@
+<?php
+
+  include ("../inc/usarBD.php");
+
+  $consulta = "SELECT * FROM languages";
+
+  $result = mysqli_query($conexion, $consulta);
+
+  $sJSON='[';
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      $sJSON .= '{"id":'. $row["id"].',"language_name":"'. $row["language_name"].'"},'  ;
+    }
+  }
+  $sJSON = rtrim($sJSON,",");
+  $sJSON.=']';
+
+  echo $sJSON;
+
+  mysqli_close($conexion);
+
+ ?>
