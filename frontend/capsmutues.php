2ef517ec5e1fd4776c27e92bf867dc598049c216
Víctor <victor.vivancos@hotmail.com>
2019-05-22 20:04:46 +0200

    Updated merge conflict

--- frontend/capsmutues.php
+++ frontend/capsmutues.php
@@ -206,8 +206,6 @@
         </table>
     </div>
   </div>
-<<<<<<< HEAD
-=======
     <!-- Div afegir  cap -->
     <div class="container" id="addCap">
       <button id="showListBtn" type="button" class="btn btn-primary">Tornar a la llista</button>
@@ -237,7 +235,7 @@
     </div>
 
     <!-- Div modificar usuari -->
-    <div class="container" id="modCapDiv">
+    <!-- <div class="container" id="modCapDiv">
       <button id="showListBtnMod" type="button" class="btn btn-primary">Tornar a la llista</button>
       <form id="formModCap">
         <div class="form-group">
@@ -262,7 +260,7 @@
           </div>
         </div>
       </form>
-    </div>
+    </div> -->
 
     <!--Modal Delete cap-->
     <div class="modal fade" id="deletecapmodal" tabindex="-1" role="dialog">
@@ -287,7 +285,6 @@
 
 
 
->>>>>>> fe02fcc30620d04244520378e97158fccacfc471
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
