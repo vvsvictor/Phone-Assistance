0797e737cdd625fafbfb16916c521307bf533d86
Daesca99 <daesca@bemen3.cat>
2019-05-25 17:57:43 +0200

    Change sql & add subconsultas (Por arreglar)

--- backend/selects/getCallHistory.php
+++ backend/selects/getCallHistory.php
@@ -1,23 +1,51 @@
 <?php
 
   include ("../inc/usarBD.php");
-
   $consulta = "SELECT * FROM CALL_HISTORY";
-
   $result = mysqli_query($conexion, $consulta);
-
   $sJSON='[';
   if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-      $sJSON .= '{"id":'. $row["id"].',"user_dninif":"'. $row["user_dninif"].'","call_date":"'. $row["call_date"].'","call_type":'. $row["call_type"].',"outcall_type":'. $row["outcall_type"].',"incall_type":'. $row["incall_type"].',"call_state":'. $row["call_state"].',"teleoperator_solution":"'. $row["teleoperator_solution"].'"},'  ;
-    }
+      $sJSON .= '{"id":'. $row["id"].',"user_dninif":"'. $row["user_dninif"].'","call_date":"'. $row["call_date"].'","teleoperator_solution":"'. $row["teleoperator_solution"].'", '  ;
+
+
+      $consultaType = 'SELECT call_type FROM call_type WHERE  id = (SELECT call_type from call_history where call_type = '.$row["id"].')';
+      $resultType = mysqli_query($conexion, $consultaType);
+      if (mysqli_num_rows($resultType) > 0) {
+        while ($rowType = mysqli_fetch_assoc($resultType)) {
+          $sJSON .= '"call_type":"'. $rowType["call_type"].'", ';
+        }
+      }
+
+      $consultaState = 'SELECT call_type FROM call_state WHERE  id = (SELECT call_state from call_history where call_state = '.$row["id"].')';
+      $resultState = mysqli_query($conexion, $consultaState);
+      if (mysqli_num_rows($resultState) > 0) {
+        while ($rowState = mysqli_fetch_assoc($resultState)) {
+          $sJSON .= '"call_state":"'. $rowState["call_state"].'", ';
+        }
+      }
+
+      $consultaOutcall = 'SELECT outcall_type FROM outcall_type WHERE  id = (SELECT outcall_type from call_history where outcall_type = '.$row["id"].')';
+      $resultOutcall = mysqli_query($conexion, $consultaOutcall);
+      if (mysqli_num_rows($resultOutcall) > 0) {
+        while ($rowOutcall = mysqli_fetch_assoc($resultOutcall)) {
+          $sJSON .= '"outcall_type":"'. $rowState["outcall_type"].'", ';
+        }
+      }
+
+      $consultaIncall = 'SELECT incall_type FROM incall_type WHERE  id = (SELECT incall_type from call_history where incall_type = '.$row["id"].')';
+      $resultIncall = mysqli_query($conexion, $consultaIncall);
+      if (mysqli_num_rows($resultIncall) > 0) {
+        while ($rowIncall = mysqli_fetch_assoc($resultIncall)) {
+          $sJSON .= '"outcall_type":"'. $rowState["outcall_type"].'"},';
+        }
+      }
   }
+}
+
   $sJSON = rtrim($sJSON,",");
   $sJSON.=']';
-
   echo $sJSON;
-
   mysqli_close($conexion);
-
  ?>
--- frontend/js/histocalls.js
+++ frontend/js/histocalls.js
@@ -27,56 +27,6 @@ function showTable(){
   });
 }
 
-function showState() {
-  $.ajax({
-    url: "../backend/selects/getCallState.php",
-    type: "GET",
-    cache: false,
-    success: function(response) {
-      let myJSON = JSON.parse(response);
-      let provincias = [];
-      for (var i = 0; i < myJSON.length; i++) {
-        let province = myJSON[i].prov_name;
-        let id = myJSON[i].id;
-        provincias.push("(Id:" + id + ") " + province);
-      }
-      $("#divComarcas").hide();
-      $("#divMunicipios").hide();
-      $("#provincias").kendoAutoComplete({
-        filter: "contains",
-        dataSource: provincias,
-        placeholder: "Selecciona una provincia...",
-      });
-    },
-    error: function() {
-      console.log('No hi han provincies');
-    }
-  });
-}
-
-function showLanguages(){
-  $.ajax({
-    url: "../backend/selects/getLanguages.php",
-    type: "GET",
-    cache: false,
-    success: function(response) {
-      let myJSON = JSON.parse(response);
-      html="";
-      for (let i = 0; i < myJSON.length; i++) {
-        html+='<option>(Id:'+myJSON[i].id+") "+myJSON[i].language_name+'</option>';
-      }
-      html+="<option>Altre</option>";
-      $("#addIdioma").html(html);
-      $("#addIdioma").kendoDropDownList();
-      $("#addIdiomaAltre").hide();
-    },
-    error: function() {
-      console.log('No hi han llenguatges');
-    }
-  });
-}
-
-
 function showHistoCall(id, dni, name, date, state){
   let html="<tr><td>"+id+"</td><td>"+dni+"</td><td>"+date+"</td><td>"+state+"</td><td><button id='histoCall" + id + "' type='button' class='histoCall btn btn-info marginBtn'>Fitxa Completa</button><button type='button' id='deleteCallId" + id + "' class='deletecall btn btn-danger marginBtn' data-toggle='modal' data-target='#deletecallmodal'>Eliminar</button></td></tr>";
   $("#histoCallsTable").append(html);
