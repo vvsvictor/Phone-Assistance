818c36e97e31dba78051fadc521e724ff54c184e
sengi718 <martinez.garcia.jordi@gmail.com>
2019-05-25 23:06:49 +0200

    solved getDoctors.php now get the name of specialization not the id

--- backend/selects/getDoctors.php
+++ backend/selects/getDoctors.php
@@ -10,7 +10,15 @@
   if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-      $sJSON .= '{"id":'. $row["id"].',"name":"'. $row["name"].'","surname":"'. $row["surname"].'","gender":"'. $row["gender"].'","specialization_id":'. $row["specialization_id"].',"id_cap":'. $row["id_cap"].'},'  ;
+      $sJSON .= '{"id":'. $row["id"].',"name":"'. $row["name"].'","surname":"'. $row["surname"].'","gender":"'. $row["gender"].'", "id_cap":'. $row["id_cap"].', ';
+
+      $consultaSpecialization = 'SELECT med_specialization FROM med_specialization WHERE id = (SELECT specialization_id from doctors where id = '.$row["id"].')';
+      $resultSpecialization = mysqli_query($conexion, $consultaSpecialization);
+      if (mysqli_num_rows($resultSpecialization) > 0) {
+        while ($rowSpecialization = mysqli_fetch_assoc($resultSpecialization)) {
+          $sJSON .= '"med_specialization":"'. $rowSpecialization["med_specialization"].'"},';
+        }
+      }
     }
   }
   $sJSON = rtrim($sJSON,",");
