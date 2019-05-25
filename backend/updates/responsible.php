1bcd3552da7bae35b4b6acf9b7dae08f0ada347f
sengi718 <martinez.garcia.jordi@gmail.com>
2019-05-25 18:57:42 +0200

    update responsible

---
+++ backend/updates/responsible.php
@@ -0,0 +1,38 @@
+<?php
+
+  include ("../inc/usarBD.php");
+  $id = $_GET["id"];
+  $sUser_dninif = $_GET["sUser_dninif"];
+  $sPrority = $_GET["$sPrority"];
+  $sName = $_GET["sName"];
+  $sSurname = $_GET["sSurname"];
+  $sAddress = $_GET["sAddress"];
+  $sPostcode = $_GET["sPostcode"];
+  $sContact_phone = $_GET["sContact_phone"];
+  $sPreferable_hour = $_GET["sPreferable_hour"];
+  $sDate_responsible = $_GET["sDate_responsible"];
+  $sReason = $_GET["sReason"];
+
+
+  $consulta="UPDATE responsible SET user_dninif='".$sUser_dninif."', priority='".$sPrority."', name='".$sName."', surname='".$sSurname."', address='".$sAddress."', post_code='".$sPostcode."', contact_phone='".$sContact_phone."', preferable_hour='".$sPreferable_hour."', reason='".$sReason."' WHERE id=".$id;
+  $hacerConsulta=mysqli_query($conexion, $consulta);
+
+  $sJSON = "[{";
+
+  if ($hacerConsulta){
+	  $sJSON .= '"codigoError": 0,';
+	  $sJSON .= '"descError": "",';
+	  $sJSON .= '"observaciones": "Todo OK"';
+  } else {
+	  $sJSON .= '"codigoError": -1,';
+	  $sJSON .= '"descError": "Error en la consulta",';
+	  $sJSON .= '"observaciones": "KO!"';
+  }
+
+  $sJSON .= "}]";
+
+  echo $sJSON;
+
+  @mysqli_free_result ($hacerConsulta);
+  mysqli_close ($conexion);
+?>
