a7a8d15bc438f7ad6202edc6de975b07c95a4272
Víctor <victor.vivancos@hotmail.com>
2019-05-28 16:36:40 +0200

    Updated historial trucades form

--- backend/selects/getIncallType.php
+++ backend/selects/getIncallType.php
@@ -10,7 +10,7 @@
   if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-      $sJSON .= '{"id":'. $row["id"].',"incall_type":"'. $row["incall_type"].'"},'  ;
+      $sJSON .= '{"id":'. $row["id"].',"incall_type":"'. $row["incall_type"].'","subclass":"'. $row["subclass"].'"},'  ;
     }
   }
   $sJSON = rtrim($sJSON,",");
