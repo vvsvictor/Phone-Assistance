5b851e4e1208804e356362820d6c62bc3b777fe4
Víctor <victor.vivancos@hotmail.com>
2019-05-30 14:26:32 +0200

    Show calls error

--- frontend/js/histocalls.js
+++ frontend/js/histocalls.js
@@ -240,7 +240,7 @@ function mostrarCallListener() {
       if ($("#addtype_list").val()=="Entrant") {
         //Tipus de trucada entrant
         let incall = $("#addentrant_call").data("kendoDropDownTree").value().text;
-
+        $.ajax({
           url: "../backend/inserts/insertCallHistory.php",
           data: {
             $sDniNif: dni,
