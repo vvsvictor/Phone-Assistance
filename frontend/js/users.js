37c274a68e75441a369104d7831274c93ee21428
Eduardo Torres <edtoar@bemen3.cat>
2019-05-31 19:32:03 +0200

    correction on bootstrap validator

--- frontend/js/users.js
+++ frontend/js/users.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
         },
         fields: {
           username: {
